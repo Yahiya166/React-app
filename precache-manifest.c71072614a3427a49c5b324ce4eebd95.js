d31b7d766189bb7bd7e28a6651fc818c1518e57d
Yahiya166 <muhammadyahiya166@gmail.com>
2020-11-05 15:13:39 +0500

    Updates

---
+++ precache-manifest.c71072614a3427a49c5b324ce4eebd95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9043480d966d79225bea178bdda617e0",
+    "url": "/React-app/index.html"
+  },
+  {
+    "revision": "61d955cdb7b2ee1789d4",
+    "url": "/React-app/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "1890ff87cf2b8ee2e9c3",
+    "url": "/React-app/static/css/main.c0d7a77a.chunk.css"
+  },
+  {
+    "revision": "61d955cdb7b2ee1789d4",
+    "url": "/React-app/static/js/2.85235604.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-app/static/js/2.85235604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1890ff87cf2b8ee2e9c3",
+    "url": "/React-app/static/js/main.7206a6b7.chunk.js"
+  },
+  {
+    "revision": "9498bf116b11b7316f6c",
+    "url": "/React-app/static/js/runtime-main.53325975.js"
+  },
+  {
+    "revision": "aaa28d6b99fed31e275309cc230e37fd",
+    "url": "/React-app/static/media/logo-transparent.aaa28d6b.png"
+  }
+]);
